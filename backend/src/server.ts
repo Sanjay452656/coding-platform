5e8abeb346fd3d1ebd0cb09168b70f7d748c657f
Sanjay Birla <birlasanjay555@gmail.com>
2026-06-20 01:05:43 +0530

    prisma setup done now db connected

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,10 +2,23 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import app from './app.js';
+import prisma from './utils/prisma.js'
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+async function startServer(){
+    try {
+        await prisma.$connect();
+        console.log("Database connected");
+
+        app.listen(PORT,()=>{
+            console.log(`server is running on port ${PORT}`)
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startServer();
 
