8927fbe0ed39c520a4c347c4527a045f8a6c8d3c
Sanjay Birla <birlasanjay555@gmail.com>
2026-06-19 23:58:27 +0530

    health route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import cors from 'cors'
+import healthRoute from './routes/health.route.js'
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+app.use('/health' , healthRoute)
 
 app.get('/',(req, res) => {
     res.send("Hello World!");
